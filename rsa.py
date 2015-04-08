3dc5ab370940a02f39dfb115226d28b7ab1ab1d4
reedie <ehogle@uoregon.edu>
2015-04-08 16:26:38 -0700

    latest rsa py

--- rsa.py
+++ rsa.py
@@ -1,6 +1,25 @@
-def randomprime():
-    return 8675309
-    #xkcd.com/221/
+import random
+#there are more secure random number generating modules one should use in practice.
+
+def exp(b,p,n):
+	#bin=p in binary
+	#for d in digits(bin) (right to left)
+		#if d==1 then
+			# r=r*s % n
+		#s=s^2 %n
+	# return r
+    return (b**p)%n
+
+
+def randomprime(n):
+	#returns a random prime less than n
+	if n<10: return "ERROR"
+	while 1>0:
+		guess=random.randint(3,n)
+		if {exp(k,guess-1,guess) for k in [2,3,5,7]}=={1L}:
+			return guess
+			break
+
     
 def gcd(x,y):
     if y==0:
@@ -8,27 +27,27 @@ def gcd(x,y):
     else:
         return gcd(y,x % y)
 
-def exp(b,p,n):
-    #returns b to the p mod n. Needs to be made much more efficient.
-    return (b**p)%n
-    
-def ext_euclid(x,y):
-    #returns a,b, and d such that gcd(x,y)=d=ax+by.
-    return 0,0,gcd(x,y) #one out of three... 
+
+def ext_euclid(a,b):
+	#returns a,b, and d such that gcd(x,y)=d=ax+by.
+	#From Dasgupta's Algorithms book
+	if b==0:
+		return 1,0,a
+	x,y,d=ext_euclid(b,a-b*(a/b))
+	return y, x-(a/b)*y,d
+
 
 def inverse(g,n):
     #returns h so that g*h=1 mod n
     #first check that g is an element of the multiplicative group
     if gcd(g,n)!=1: print "ERROR!"
     a,b,d=ext_euclid(g,n)
-    #so ag+bn=1, which means ag=1 mod n.
-    return a
+    #so ag+bn = 1, which means ag = 1 (mod n).
+    return a % n
 
-def genkey():
-    p=randomprime()
-    q=randomprime()
-    p=11 #delete once randomprime actually works
-    q=7  #delete once randomprime actually works
+def genkey(n):
+    p=randomprime(2**n)
+    q=randomprime(2**n)
     n=p*q
     phi=(p-1)*(q-1)
     e=2**16+1
@@ -64,11 +83,20 @@ def unchunk(a):
 	return s
 
 s="This is some text that I'm chunking then unchunking."
+print s,"\n"
 a=chunk(s)
 print a,"\n"
 t=unchunk(a)	
-print s
-print t	
+print t,"\n"
 
-	
-# genkey()
+# print randomprime(10)
+print randomprime(100)
+print randomprime(1000)
+print randomprime(10000)
+print randomprime(100000)
+print randomprime(1000000)
+# print randomprime(10000000)
+# print randomprime(100000000)
+
+print	
+genkey(10)
